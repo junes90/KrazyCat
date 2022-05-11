66b0be6f27785a32293f779912725a9f18a04efd
Junes90 <junes90@icloud.com>
2022-05-11 17:20:57 +0900

    Update yum_install.sh

--- yum_install.sh
+++ yum_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#yum pgk install
-echo `yum update -y`
+echo `mkdir test`
+echo `sudo yum update -y`
 PKG=$(cat yum_install)
-echo `yum install -y ${PKG}`
-echo `yum groupinstall -y "Development Tools"`
+#echo ${PKG}
+echo `sudo yum install -y ${PKG}`
+echo `sudo yum groupinstall -y "Development Tools"`
 
-#python donwload
 echo `wget https://www.python.org/ftp/python/3.7.13/Python-3.7.13.tgz`
 echo `tar -zxvf Python-3.7.13.tgz`
